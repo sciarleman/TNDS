753d7abf6df5b9df69ef8184d11d288240353bbe
sciarleman <115356440+sciarleman@users.noreply.github.com>
2023-03-02 14:47:13 +0100

    Create (5.3) PuntoMateriale.h

---
+++ (5.3) PuntoMateriale.h
@@ -0,0 +1,23 @@
+#ifndef __PuntoMateriale_h__
+#define __PuntoMateriale_h__
+
+#include "Particella.h"
+#include "Posizione.h"
+
+#include "CampoVettoriale.h"
+
+class PuntoMateriale : public Particella, Posizione {
+
+public:
+
+  PuntoMateriale(double massa, double carica, const Posizione&);
+  PuntoMateriale(double massa, double carica, double x, double y, double z);
+ // ~PuntoMateriale();
+
+  CampoVettoriale CampoElettrico(const Posizione&) const ;
+  CampoVettoriale CampoGravitazionale(const Posizione&) const;
+
+};
+
+#endif // __PuntoMateriale_h__
+
