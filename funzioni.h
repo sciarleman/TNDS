ea2248e1374675feffa2bbe7a4ed9d09de7060b0
sciarleman <115356440+sciarleman@users.noreply.github.com>
2023-03-02 14:03:15 +0100

    Create funzioni.h

---
+++ funzioni.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+#include "funzioni.cpp"
+
+using namespace std;
+
+double read (const string *, int);
+double calcmedia (double *, int);
+double calcvarianza (double *, int);
+double riordina (double, double);
+double calcmediana (double [], int);
+void print (const char*, double*, int);
+void print (double*, int);
