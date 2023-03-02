3121c366e4d056835da77ec7eb427739d701d555
sciarleman <115356440+sciarleman@users.noreply.github.com>
2023-03-02 14:44:33 +0100

    Create (5.3) esercizio_5.3.cpp

---
+++ (5.3) esercizio_5.3.cpp
@@ -0,0 +1,34 @@
+#ifndef __CampoVettoriale_h__
+#define __CampoVettoriale_h__
+
+#include "Posizione.h"
+#include<iostream>
+using namespace std;
+
+
+class CampoVettoriale : public Posizione {
+
+public:
+
+  CampoVettoriale(const Posizione&);
+  CampoVettoriale(const Posizione&, double Fx, double Fy, double Fz);
+  CampoVettoriale(double x, double y, double z, double Fx, double Fy, double Fz);
+  ~CampoVettoriale();
+
+  CampoVettoriale & operator+=( const CampoVettoriale & ) ;
+  CampoVettoriale operator+( const CampoVettoriale & ) const;
+  
+  double GetFx() const {return dim_Fx;}
+  double GetFy() const {return dim_Fy;}
+  double GetFz() const {return dim_Fz;}
+
+  double Modulo();
+	void SetComponent(double, double, double);
+		
+private:
+
+  double dim_Fx, dim_Fy, dim_Fz;
+
+};
+
+#endif // __CampoVettoriale_h__ 
